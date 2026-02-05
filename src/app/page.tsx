71e4ac8a98481750b4c7079b5c675f46f84780ca
rukundowilson <rukundowilson5@gmail.com>
2026-02-05 11:44:38 +0200

    project structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans">
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white sm:items-start">
-        <h1 className="text-red-500">Hello world</h1>
+        <h1 className="text-red-500">Hello world, well come</h1>
       </main>
     </div>
   );
