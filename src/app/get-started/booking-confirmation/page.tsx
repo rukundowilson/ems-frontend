d82a25b0c743e7a65a7c212776d8cd842d3e88e7
Assumpta-tech <uwamariya.assumpta@yahoo.com>
2026-02-11 12:23:14 +0200

    conflict resolved successfully

--- src/app/get-started/booking-confirmation/page.tsx
+++ src/app/get-started/booking-confirmation/page.tsx
@@ -83,11 +83,7 @@ export default function BookingConfirmationPage() {
   }
 
   return (
-<<<<<<< HEAD
-    <div className="min-h-screen bg-gray-50">
-=======
     <div className="min-h-screen bg-gray-100">
->>>>>>> main
         <Header/>
       <div className="max-w-6xl mx-auto my-12">
         {/* Header */}
@@ -207,10 +203,7 @@ export default function BookingConfirmationPage() {
           </div>
         </div>
       </div>
-<<<<<<< HEAD
-=======
       <br />
->>>>>>> main
     </div>
   );
 }
